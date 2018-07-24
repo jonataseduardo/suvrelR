2399e12013fc776543bedb8dba5cd4acf56756a4
Jonatas Cesar <jonataseduardo@gmail.com>
2018-07-23 19:54:46 -0700

    X data

--- vignettes/geuvadis.R
+++ vignettes/geuvadis.R
@@ -4,23 +4,27 @@ library(ggplot2)
 devtools::document('~/suvrelR')
 devtools::load_all('~/suvrelR')
 
-#fname <- paste0(system.file("extdata", package = "diverdt"), 
-#                '/', pop_name, '_Illumina')
 sample_info <- 
-  fread('/raid/genevol/users/vitor/hlaexpression/geuvadis_reanalysis/data/sample_info/geuvadis_sample_info.tsv')
+  fread('/home/jonatas/suvrelR/extdata/geuvadis_sample_info.tsv')
 
 gene_long <- 
-  fread('/raid/genevol/users/vitor/hlaexpression/geuvadis_reanalysis/expression/3-map_to_transcriptome/hla_personalized/gene_quantifications.tsv') 
+  fread('/home/jonatas/suvrelR/extdata/gene_quantifications.tsv') 
 
 sample_info
+
 datum <- 
   gene_long[sample_info[,.(name, pop)], 
             on = c(subject = 'name')]
 
-devtools::load_all('~/suvrelR')
-erg <- suvrel_diag(datum, value_var = 'tpm', group = 'pop', feature = 'gene_id')
+erg <- suvrel_diag(datum, 
+                   value_var = 'tpm', 
+                   group = 'pop', 
+                   feature = 'gene_id')
 
 erg[,summary(g)]
 new_datum <- datum[erg, on = .(gene_id)]
-new_datum
+new_datum[, tpm_g := tpm * sqrt(g)]
+
+X_old <- dcast(new_datum, gene_id ~ subject, value.var = 'tpm')
+X_new <- dcast(new_datum, gene_id ~ subject, value.var = 'tpm_g')
 
