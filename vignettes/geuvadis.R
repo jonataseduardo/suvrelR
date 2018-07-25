22d9d2c76301e7dce76f14e07048a64a994bfb5a
Jonatas Cesar <jonataseduardo@gmail.com>
2018-07-24 18:28:10 -0700

    using geuvadis quantification and removing ceu and non-autosomal expression

--- vignettes/geuvadis.R
+++ vignettes/geuvadis.R
@@ -7,24 +7,35 @@ devtools::load_all('~/suvrelR')
 sample_info <- 
   fread('/home/jonatas/suvrelR/extdata/geuvadis_sample_info.tsv')
 
+gene_wide <- 
+  fread('/home/jonatas/suvrelR/extdata/geuvadis_quantification.txt') 
+
 gene_long <- 
-  fread('/home/jonatas/suvrelR/extdata/gene_quantifications.tsv') 
+  melt(gene_wide, measure.vars = 5:dim(gene_wide)[2], 
+       variable.name = 'subject', 
+       value.name = 'fpkm')
 
 datum <- 
   gene_long[sample_info[,.(name, pop)], 
-            on = c(subject = 'name')]
+            on = c(subject = 'name')
+            ][pop != 'CEU' && !is.na(Chr)]
+
+datum[, Chr := as.integer(Chr)]
+datum <- datum[!is.na(Chr)]
+
+setnames(datum, 'TargetID', 'gene_id')
 
 erg <- suvrel_diag(datum, 
-                   value_var = 'tpm', 
+                   value_var = 'fpkm', 
                    group = 'pop', 
                    feature = 'gene_id')
-
 erg[,summary(g)]
-new_datum <- datum[erg, on = .(gene_id)]
-new_datum[, tpm_g := tpm * sqrt(g)]
 
-X_old <- dcast(new_datum, subject + pop ~ gene_id, value.var = 'tpm')
-X_new <- dcast(new_datum, subject + pop ~ gene_id, value.var = 'tpm_g')
+new_datum <- datum[erg, on = .(gene_id)]
+new_datum[, fpkm_g := fpkm * sqrt(g)]
+
+X_old <- dcast(new_datum, subject + pop ~ gene_id, value.var = 'fpkm')
+X_new <- dcast(new_datum, subject + pop ~ gene_id, value.var = 'fpkm_g')
 n <- dim(X_old)[2]
 
 pc_old <- prcomp(X_old[,3:n], center = TRUE)
@@ -32,8 +43,7 @@ po <- as.data.table(pc_old$rotation)[, 1:4]
 
 pc_new <- prcomp(X_new[,3:n], center = TRUE)
 pn <- as.data.table(pc_new$rotation)[, 1:4]
+X_new[,1:5]
 
-ggplot(pn) + 
+ggplot(po) + 
   geom_point(aes(x = PC1, y = PC2))
-
-
